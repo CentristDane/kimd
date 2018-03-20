4c8939d4105b870a42523b18083383a5f197755d
Dane Sherrets <Dane@Danes-MacBook-Air.local>
2018-03-20 12:17:58 -0400

    first draft

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron text-center">
+    <h1>It's not delivery, it's DiGioia</h1>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="http://www.recipepuppy.com/about/api/"
+    >
+      For the busy PhD student that hates grocery shopping
+    </a>
+  </div>
+);
+
+export default Jumbotron;
